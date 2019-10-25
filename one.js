b84369f14303ea3bf26edad1343d81347f9ba7bf
Kenneth-joseph <josephochieng812@gmail.com>
2019-10-25 17:55:09 +0300

    added my variables to my  js by use of id's

--- one.js
+++ one.js
@@ -1,2 +1,18 @@
-var maleName =["kwasi", "kwadwo","kwabena","kwaku","yaw","kofi","kwame"];
-var femaleName=["Akosua", "Adwao","Abenaa", "Akua","Yaa","Afua"];
+
+function oncalculate()
+{
+    var maleName =["kwasi", "kwadwo","kwabena","kwaku","yaw","kofi","kwame"];
+    var femaleName=["Akosua", "Adwao","Abenaa", "Akua","Yaa","Afua"];
+
+    var dayOfBirth = parseInt(document.getElementById("day").value);
+    var monthOfBirth= parseInt(document.getElementById("month").value);
+    var yearOfBirth= document.getElementById("year").value;
+
+
+    var y= yearOfBirth.slice(2,4);
+    var c= yearOfBirth.slice(0,2);
+    alert(y);
+    alert(c);
+    
+    
+}
