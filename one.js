79c3de39cce0063da47ab9eb6b0c3076c1cbcb09
Kenneth-joseph <josephochieng812@gmail.com>
2019-10-26 17:10:53 +0300

    added my caculation function

--- one.js
+++ one.js
@@ -1,18 +1,26 @@
 
 function oncalculate()
 {
-    var maleName =["kwasi", "kwadwo","kwabena","kwaku","yaw","kofi","kwame"];
-    var femaleName=["Akosua", "Adwao","Abenaa", "Akua","Yaa","Afua"];
+    // var maleName =["kwasi", "kwadwo","kwabena","kwaku","yaw","kofi","kwame"];
+    // var femaleName=["Akosua", "Adwao","Abenaa", "Akua","Yaa","Afua"];
 
     var dayOfBirth = parseInt(document.getElementById("day").value);
     var monthOfBirth= parseInt(document.getElementById("month").value);
     var yearOfBirth= document.getElementById("year").value;
 
 
-    var y= yearOfBirth.slice(2,4);
-    var c= yearOfBirth.slice(0,2);
-    alert(y);
-    alert(c);
+    var sliceY= yearOfBirth.slice(2,4);
+    var sliceC= yearOfBirth.slice(0,2);
+    var c= parseInt(sliceC);
+    var y= parseInt(sliceY);
+    var d= dayOfBirth;
+    var m=monthOfBirth;
+
+   
+    // var dayOfTheWeek = ( ( (c/4) -2*c-1) + ((5*y/4) ) + ((26*(m+1)/10)) + d )% 7
+   var dayOfTheWeek= ( ( (c/4) - 2 * c - 1) + ((5*y/4) ) + ((26*(m+1)/10)) + d ) %7;
+   alert(dayOfTheWeek);
+  
     
     
 }
