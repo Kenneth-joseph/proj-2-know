c54072027e60e2491ed6bad55f31c2600312b3de
Kenneth-joseph <josephochieng812@gmail.com>
2019-10-27 16:27:26 +0300

    added my validation code and my male array function to the perfection

--- one.js
+++ one.js
@@ -1,13 +1,35 @@
 
+// function verifyNumber( myInput ){
+//     if( !isNaN( parseFloat( myInput.value ) ) && isFinite( myInput.value ) )
+//         return true;
+//     else
+//         return false;
+// }
+
+
+
+
+
 function oncalculate()
 {
-    // var maleName =["kwasi", "kwadwo","kwabena","kwaku","yaw","kofi","kwame"];
-    // var femaleName=["Akosua", "Adwao","Abenaa", "Akua","Yaa","Afua"];
+    var maleName =["kwasi", "kwadwo","kwabena","kwaku","yaw","kofi","kwame"];
+    var femaleName=["Akosua", "Adwao","Abenaa", "Akua","Yaa","Afua"];
 
     var dayOfBirth = parseInt(document.getElementById("day").value);
     var monthOfBirth= parseInt(document.getElementById("month").value);
     var yearOfBirth= document.getElementById("year").value;
-
+     
+    
+    if( dayOfBirth <= 0 || dayOfBirth>31)
+     {
+         alert("enter valid date"); 
+         return false;
+     }
+      else if(monthOfBirth <=0 || monthOfBirth > 12)
+      {
+          alert("enter valid month");
+          return false;
+      }
 
     var sliceY= yearOfBirth.slice(2,4);
     var sliceC= yearOfBirth.slice(0,2);
@@ -17,10 +39,13 @@ function oncalculate()
     var m=monthOfBirth;
 
    
-    // var dayOfTheWeek = ( ( (c/4) -2*c-1) + ((5*y/4) ) + ((26*(m+1)/10)) + d )% 7
-   var dayOfTheWeek= ( ( (c/4) - 2 * c - 1) + ((5*y/4) ) + ((26*(m+1)/10)) + d ) %7;
-   alert(dayOfTheWeek);
-  
-    
+   var dayOfTheWeek= Math.round(( ( (c/4) - 2 * c - 1) + ((5*y/4) ) + ((26*(m+1)/10)) + d ) %7);
+
+  for(var a=0;a< maleName.length;a++){
+      if(a===dayOfTheWeek){
+          alert(maleName[a]);
+      }
+  }
+//   var gender;
     
 }
